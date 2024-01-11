905ee88273fe8a862b1f4cce0811ae8135edaa5e
StopTheClocks <stoptheclocksforever@yandex.ru>
2024-01-11 22:16:29 +0300

    Test message

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,13 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FilmController {
+
+    @GetMapping("/test")
+    public String homePage() {
+        return "Тест";
+    }
 }
