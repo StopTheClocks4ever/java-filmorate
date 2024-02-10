965a9a89a2ad3a6327e09911408fca6a472690b4
StopTheClocks <stoptheclocksforever@yandex.ru>
2024-02-10 17:04:18 +0300

    FZ10 Commit-1

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -4,13 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
 public class Film {
-    private int id;
+    private long id;
     private final String name;
     private final String description;
     private final LocalDate releaseDate;
     private final Integer duration; //продолжительность в минтутах
+    private Set<Long> likes;
+    private long likesSum;
 }
